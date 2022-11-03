1856a28ec4657766ce30f1d2261c3b63e2cc5453
Daniel Artine Mastub <daniel.mastub@stoneage.com.br>
2022-11-02 21:10:03 -0300

    Aula-2

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -9,32 +9,13 @@ public class FilmeController : ControllerBase
 {
 
     private static List<Filme> filmes = new List<Filme>();
-    private static int id = 0;
 
     [HttpPost]
-    public IActionResult AdicionaFilme(
-        [FromBody] Filme filme)
+    public void AdicionaFilme([FromBody] Filme filme)
     {
-        filme.Id = id++;
         filmes.Add(filme);
-        return CreatedAtAction(nameof(RecuperaFilmePorId),
-            new { id = filme.Id },
-            filme);
-    }
-
-    [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, 
-        [FromQuery] int take = 50)
-    {
-        return filmes.Skip(skip).Take(take);
-    }
-
-    [HttpGet("{id}")]
-    public IActionResult RecuperaFilmePorId(int id)
-    {
-        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
-        if (filme == null) return NotFound();
-        return Ok(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
     }
 }
         
